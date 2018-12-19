2b028773199b4478961960be7982d45c0e18d3be
Ali Shanaakh <ashanaakh@gmail.com>
2018-12-20 00:49:18 +0200

    Refactor usage of sql for future tests

--- src/main.go
+++ src/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"fmt"
+)
+
+import (
+	"github.com/jmoiron/sqlx"
 	"github.com/palestine-nights/backend/src/api"
+	"github.com/palestine-nights/backend/src/db"
 	"github.com/palestine-nights/backend/src/tools"
 )
 
+func initializeDB() *sqlx.DB {
+	user := tools.GetEnv("DATABASE_USER", "root")
+	pass := tools.GetEnv("DATABASE_PASSWORD", "")
+	name := tools.GetEnv("DATABASE_NAME", "restaurant")
+	host := tools.GetEnv("DATABASE_HOST", "localhost")
+	port := tools.GetEnv("DATABASE_PORT", "3306")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
+
+	return db.Initialize(connectionString)
+}
+
 func main() {
-	databaseUser := tools.GetEnv("DATABASE_USER", "root")
-	databasePassword := tools.GetEnv("DATABASE_PASSWORD", "")
-	databaseName := tools.GetEnv("DATABASE_NAME", "restaurant")
-	databaseHost := tools.GetEnv("DATABASE_HOST", "localhost")
-	databasePort := tools.GetEnv("DATABASE_PORT", "3306")
-
-	server := api.GetServer(
-		databaseUser,
-		databasePassword,
-		databaseName,
-		databaseHost,
-		databasePort,
-	)
-
+	DB := initializeDB()
+	server := api.GetServer(DB)
 	server.ListenAndServe()
 }
