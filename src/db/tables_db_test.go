15e3f3e6da47922277c408e5926b9c08a44f947a
Ali Shanaakh <github@shanaakh.pro>
2018-12-20 01:49:11 +0200

    Add more tests (#40)

--- src/db/tables_db_test.go
+++ src/db/tables_db_test.go
@@ -1,15 +1,18 @@
 package db
 
 import (
-	"github.com/jmoiron/sqlx"
-	"github.com/stretchr/testify/suite"
-	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"fmt"
 	"testing"
 )
 
-// Define the suite, and absorb the built-in basic suite
-// functionality from testify - including a T() method which
-// returns the current testing context
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+/* --- Suite 1 --- */
+
 type GetTablesSuite struct {
 	suite.Suite
 	Expected []Table
@@ -17,8 +20,6 @@ type GetTablesSuite struct {
 	Mock     sqlmock.Sqlmock
 }
 
-// Make sure that VariableThatShouldStartAtFive is set to five
-// before each test
 func (suite *GetTablesSuite) SetupTest() {
 	db, mock, err := sqlmock.New()
 
@@ -44,9 +45,7 @@ func (suite *GetTablesSuite) SetupTest() {
 	}
 }
 
-// All methods that begin with "Test" are run as tests within a
-// suite.
-func (suite *GetTablesSuite) GetTables() {
+func (suite *GetTablesSuite) TestGetTables() {
 	defer suite.DB.Close()
 
 	rows := sqlmock.NewRows([]string{"id", "places", "description", "active"})
@@ -76,6 +75,81 @@ func (suite *GetTablesSuite) GetTables() {
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestGetTablesSuiteSuite(t *testing.T) {
+func TestGetTablesSuite(t *testing.T) {
 	suite.Run(t, new(GetTablesSuite))
+}
+
+/* --- Suite 2 --- */
+
+type FindTableSuite struct {
+	suite.Suite
+	Expected Table
+	DB       *sqlx.DB
+	Mock     sqlmock.Sqlmock
+}
+
+func (suite *FindTableSuite) SetupTest() {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		suite.T().Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	suite.Mock = mock
+	suite.DB = sqlx.NewDb(db, "sqlmock")
+	suite.Expected = Table{
+		ID:          1,
+		Places:      2,
+		Description: "Fake Table 1",
+		Active:      true,
+	}
+}
+
+func (suite *FindTableSuite) TestFindTable() {
+	defer suite.DB.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "places", "description", "active"})
+	rows.AddRow(suite.Expected.ID, suite.Expected.Places, suite.Expected.Description, suite.Expected.Active)
+
+	// Mock SQL query to return tables.
+	suite.Mock.ExpectQuery("^SELECT (.+) FROM tables WHERE id = (.+)$").WillReturnRows(rows)
+
+	// Call tested function.
+	table, err := Table.Find(Table{}, suite.DB, suite.Expected.ID)
+
+	// Throw if error.
+	if err != nil {
+		suite.T().Errorf("expected no error, but got %s instead", err)
+	}
+
+	// Make sure that all expectations were met.
+	if err := suite.Mock.ExpectationsWereMet(); err != nil {
+		suite.T().Errorf("there were unfulfilled expections: %s", err)
+	}
+
+	suite.Equal(suite.Expected, *table)
+}
+
+func (suite *FindTableSuite) TestFindTableError() {
+	defer suite.DB.Close()
+
+	suite.Mock.ExpectQuery("^SELECT (.+) FROM tables WHERE id = (.+)$").
+		WillReturnError(fmt.Errorf("SQL Error"))
+
+	// Call tested function.
+	table, err := Table.Find(Table{}, suite.DB, suite.Expected.ID)
+	suite.Equal(err.Error(),"SQL Error")
+	suite.Nil(table)
+
+	// Make sure that all expectations were met.
+	if err := suite.Mock.ExpectationsWereMet(); err != nil {
+		suite.T().Errorf("there were unfulfilled expections: %s", err)
+	}
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func
+TestFindTableSuite(t *testing.T) {
+	suite.Run(t, new(FindTableSuite))
 }
